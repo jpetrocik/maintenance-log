db0a3cd70bbc950b8544f261d1f4ad131dbc24e1
John Petrocik <john@petrocik.net>
2018-11-18 14:32:23 -0800

    Add car status

--- mysql.js
+++ mysql.js
@@ -31,7 +31,7 @@ module.exports = {
         return;     
       });
 
-      connection.query("select * from " + CAR_TABLE + " order by name desc",function(err,rows){
+      connection.query("select * from " + CAR_TABLE + " where status='ACTIVE' order by year, make, model, id, desc",function(err,rows){
         connection.release();
         callback(err, rows);
       });
