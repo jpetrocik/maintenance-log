b3b64e8aecf919629e6f64f59deebf9b566ad2f3
John Petrocik <john@petrocik.net>
2019-09-02 16:31:04 -0700

    Added support for pre-selecting car on mileage report

--- cron.js
+++ cron.js
@@ -20,7 +20,7 @@ var executeQuery = function(sqlStatement, sqlParams, callback) {
 /**
  * Registers user with a new car
  */
- let sql = 'select my_garage.id, my_garage.name as car,  user_accounts.phone as phone, datediff(now(),max(mileage_log.created_date)) as last_reported from  mileage_log join my_garage on mileage_log.carId = my_garage.id join invitations on invitations.oToken = my_garage.token join user_accounts on invitations.uToken =  user_accounts.uToken where my_garage.status=\'ACTIVE\' and invitations.role=10 group by my_garage.id, user_accounts.phone, my_garage.name having last_reported > ?'
+ let sql = 'select my_garage.id, my_garage.name as car,  user_accounts.phone as phone, user_accounts.aToken as aToken, datediff(now(),max(mileage_log.created_date)) as last_reported from  mileage_log join my_garage on mileage_log.carId = my_garage.id join invitations on invitations.oToken = my_garage.token join user_accounts on invitations.uToken =  user_accounts.uToken where my_garage.status=\'ACTIVE\' and invitations.role=10 group by my_garage.id, user_accounts.phone, user_accounts.aToken, my_garage.name having last_reported > ?'
 
 router.get('/unreported', function (req, res) {
 	executeQuery(sql, [30], (err, results) => {
@@ -36,13 +36,13 @@ router.get('/unreported', function (req, res) {
 					plivo.messages.create(
 						config.plivo.phone,
 						fields['phone'],
-						'You have not report mileage for your ' + fields['car'] + ' in over ' + fields['last_reported'] + ' days. Follow link to report mileage\n\nh' + config.host + '/mileage?noCache=1_2'
+						'You have not report mileage for your ' + fields['car'] + ' in over ' + fields['last_reported'] + ' days. Follow link to report mileage\n\n' + config.host + '/mileage?aToken=' + fields['aToken']
 					).then(function(message_created) {
 
 					});
 				}
 			} else {
-				console.log('You have not report mileage for ' + fields['car'] + ' in over ' + fields['last_reported'] + ' days.  Follow link to report mileage\n\nhttps://cars.petrocik.net/mileage');
+				console.log('You have not report mileage for your ' + fields['car'] + ' in over ' + fields['last_reported'] + ' days. Follow link to report mileage\n\n' + config.host + '/mileage?aToken=' + fields['aToken']);
 			}
 		});
 
