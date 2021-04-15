6aaadf77e2a622340d3a4f53d80c24aa444c832f
John Petrocik <john@petrocik.net>
2021-04-15 00:14:16 -0700

    Fixed cToken in url

--- cron.js
+++ cron.js
@@ -20,7 +20,13 @@ var executeQuery = function(sqlStatement, sqlParams, callback) {
 /**
  * Registers user with a new car
  */
- let sql = 'select my_garage.id, my_garage.name as car,  user_accounts.phone as phone, user_accounts.aToken as aToken, datediff(now(),max(mileage_log.created_date)) as last_reported from  mileage_log join my_garage on mileage_log.carId = my_garage.id join invitations on invitations.oToken = my_garage.token join user_accounts on invitations.uToken =  user_accounts.uToken where my_garage.status=\'ACTIVE\' and invitations.role=10 group by my_garage.id, user_accounts.phone, user_accounts.aToken, my_garage.name having last_reported > ?'
+ let sql = 'select my_garage.id, my_garage.name as car, user_accounts.phone as phone, user_accounts.aToken as aToken, \
+            invitations.iToken as cToken, datediff(now(),max(mileage_log.created_date)) as last_reported \
+            from  mileage_log join my_garage on mileage_log.carId = my_garage.id \
+            join invitations on invitations.oToken = my_garage.token \
+            join user_accounts on invitations.uToken =  user_accounts.uToken \
+            where my_garage.status=\'ACTIVE\' and invitations.role=10 \
+            group by my_garage.id, user_accounts.phone, user_accounts.aToken, invitations.iToken, my_garage.name having last_reported > ?'
 
 router.get('/unreported', function (req, res) {
 	executeQuery(sql, [30], (err, results) => {
@@ -36,13 +42,13 @@ router.get('/unreported', function (req, res) {
 					plivo.messages.create(
 						config.plivo.phone,
 						fields['phone'],
-						'You have not report mileage for your ' + fields['car'] + ' in over ' + fields['last_reported'] + ' days. Follow link to report mileage\n\n' + config.host + '/mileage?aToken=' + fields['aToken']
+						'You have not report mileage for your ' + fields['car'] + ' in over ' + fields['last_reported'] + ' days. Follow link to report mileage\n\n' + config.host + '/mileage?aToken=' + fields['aToken'] + '&cToken=' + fields['cToken']
 					).then(function(message_created) {
 
 					});
 				}
 			} else {
-				console.log('You have not report mileage for your ' + fields['car'] + ' in over ' + fields['last_reported'] + ' days. Follow link to report mileage\n\n' + config.host + '/mileage?aToken=' + fields['aToken']);
+				console.log('You have not report mileage for your ' + fields['car'] + ' in over ' + fields['last_reported'] + ' days. Follow link to report mileage\n\n' + config.host + '/mileage?aToken=' + fields['aToken'] + '&cToken=' + fields['cToken']);
 			}
 		});
 
