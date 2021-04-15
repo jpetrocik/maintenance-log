573c924ecc7fcbcfd57371c613b56d67347cbb62
John Petrocik <john@petrocik.net>
2021-04-15 01:10:16 -0700

    Added service alert

--- service_logs.js
+++ service_logs.js
@@ -64,17 +64,15 @@ let serviceLog = {
 	},
 
 	serviceDue: function(carId, callback) {
-		serviceLog.carDetails(carId, (err, carDetails) => {
-			let serviceSql = "select ms.carId, ms.id, ms.service, ms.mileage, ms.months, max(s.serviceDate) as last_service_date, max(s.mileage) as last_service_mileage, \
-				DATE_ADD(COALESCE(max(s.serviceDate),c.inserviceDate), INTERVAL months  MONTH) as due_by, \
-				DATEDIFF(DATE_ADD(COALESCE(max(s.serviceDate),c.inserviceDate), INTERVAL months  MONTH), now()) as  due_in_days, \
-				COALESCE(max(s.mileage),0)+ms.mileage-? as due_in_miles \
-				from " + CAR_DETAILS_TABLE + " c left join " + SCHEDULE_LOG_TABLE + " ms on c.id=ms.carId left outer join " + SERVICE_LOG_TABLE + " s on s.service=ms.service and \
-				s.carId=ms.carId where ms.carId=? \
-				group by ms.id, ms.service, ms.mileage, ms.months";
-			let upcomingServiceSql = "select * from (" + serviceSql + ") as s where due_in_days<30 or due_in_miles<500 order by due_in_days, due_in_miles";
-			executeQuery(upcomingServiceSql, [carDetails.mileage, carId], callback);
-		});
+		let serviceSql = "select ms.carId, ms.id, ms.service, ms.mileage, ms.months, max(s.serviceDate) as last_service_date, max(s.mileage) as last_service_mileage, \
+			DATE_ADD(COALESCE(max(s.serviceDate),c.inserviceDate), INTERVAL months  MONTH) as due_by, \
+			DATEDIFF(DATE_ADD(COALESCE(max(s.serviceDate),c.inserviceDate), INTERVAL months  MONTH), now()) as  due_in_days, \
+			COALESCE(max(s.mileage),0)+ms.mileage-c.mileage as due_in_miles \
+			from " + CAR_DETAILS_TABLE + " c left join " + SCHEDULE_LOG_TABLE + " ms on c.id=ms.carId left outer join " + SERVICE_LOG_TABLE + " s on s.service=ms.service and \
+			s.carId=ms.carId where ms.carId=? \
+			group by ms.id, ms.service, ms.mileage, ms.months";
+		let upcomingServiceSql = "select * from (" + serviceSql + ") as s where due_in_days<30 or due_in_miles<500 order by due_in_days, due_in_miles";
+		executeQuery(upcomingServiceSql, [carId], callback);
 	},
 
 	addServiceLog: function(carId, serviceDate, mileage, service, cost, note, callback) {
