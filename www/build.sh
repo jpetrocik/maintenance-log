644aeac319bee6a5d1b04178a2e156cd1dd99a73
John Petrocik <john@petrocik.net>
2023-12-29 19:26:29 -0800

    Added build script

---
+++ www/build.sh
@@ -0,0 +1,2 @@
+export PATH=/opt/node-v20.10.0-linux-x64/bin:/opt/node/bin/:/opt/node-v20.10.0-linux-x64/bin:/opt/node/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/opt/apache-maven-3.6.1//bin:/usr/lib/jvm/jdk1.8.0_112//bin
+ng build
