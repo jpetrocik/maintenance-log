815f2608d9c419df6f8d5ad61f54d69c42e8b3b5
John Petrocik <john@petrocik.net>
2023-12-29 19:27:37 -0800

    Fixed sql file exstension

--- server/src/sql/index.ts
+++ server/src/sql/index.ts
@@ -1,5 +1,5 @@
 
-import {SERVICE_DUE_SQL, UPCOMING_SERVICE_SQL} from './serviceDue.sql.js';
-import {UNREPORTED_MILEAGE_SQL} from './unreportedMileage.sql.js';
+import {SERVICE_DUE_SQL, UPCOMING_SERVICE_SQL} from './serviceDue.sql';
+import {UNREPORTED_MILEAGE_SQL} from './unreportedMileage.sql';
 
 export { SERVICE_DUE_SQL, UPCOMING_SERVICE_SQL, UNREPORTED_MILEAGE_SQL }
