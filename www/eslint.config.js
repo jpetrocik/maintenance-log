2b23f417e0692137821ad51b2063d8c1349eb43f
John Petrocik <john@petrocik.net>
2026-03-27 22:03:34 -0700

    Got lint working

--- www/eslint.config.js
+++ www/eslint.config.js
@@ -1,44 +1,37 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const { defineConfig } = require("eslint/config");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 
-module.exports = defineConfig([
+module.exports = tseslint.config(
   {
-    files: ["**/*.ts"],
+    // GLOBAL IGNORES: Only ignore these folders
+    ignores: ["dist/", ".angular/", "node_modules/"],
+  },
+  {
+    files: ["src/**/*.ts"],
     extends: [
-      eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
     rules: {
       "@angular-eslint/directive-selector": [
         "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
+        { type: "attribute", prefix: "app", style: "camelCase" },
       ],
       "@angular-eslint/component-selector": [
         "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
+        { type: "element", prefix: "app", style: "kebab-case" },
       ],
     },
   },
   {
-    files: ["**/*.html"],
+    files: ["src/**/*.html"],
     extends: [
-      angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
     ],
     rules: {},
   }
-]);
+);
