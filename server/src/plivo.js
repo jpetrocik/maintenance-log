bd8038f1e4d71f217b8a1e351a84d78859871f27
John Petrocik <john@petrocik.net>
2026-03-30 14:43:16 -0700

    Removed plivo

--- server/src/plivo.js
+++
@@ -1,7 +0,0 @@
-const config = require('./config.json');
-const plivo = require('plivo');
-
-console.log("Creating plivo client");
-let client = new plivo.Client(config.plivo.id, config.plivo.token);
-
-module.exports = client;
